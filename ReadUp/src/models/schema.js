e9f2eec0b2f915477e034da81d52f52869c14bf1
DanielleInkster <danielle.inkster@gmail.com>
2020-08-03 20:28:34 -0600

    Setup Schema

---
+++ ReadUp/src/models/schema.js
@@ -0,0 +1,17 @@
+import { appSchema, tableSchema } from "@nozbe/watermelondb";
+
+export const mySchema = appSchema({
+    version: 1,
+    tables: [
+        tableSchema({
+            name: "articles",
+            columns: [
+                { name: "title", type: "string" },
+                { name: "image", type: "string" },
+                { name: "url", type: "string" },
+                { name: "description", type: "string" },
+                { name: "date_added", type: "number" }
+            ]
+        })
+    ]
+})
