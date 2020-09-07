b53d9f9d0b55793241fdabba51d4986602fffa78
DanielleInkster <danielle.inkster@gmail.com>
2020-09-07 17:41:08 -0600

    Added description to DisplayView

--- ReadUp/src/components/DisplayView.js
+++ ReadUp/src/components/DisplayView.js
@@ -9,6 +9,7 @@ const _renderItem = ({item, index}) => {
     <View key={index} style={styles.container}>
       <Image style={styles.image} source={{uri: item.image}} />
       <Text style={styles.text}>{item.title}</Text>
+      <Text style={styles.description}>{item.description}</Text>
       <Text style={styles.link} onPress={() => Linking.openURL(item.url)}>
         Read On
       </Text>
@@ -33,23 +34,28 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#bbe1fa',
     marginTop: '3%',
-    marginBottom: '30%',
   },
   image: {
     height: Dimensions.get('window').width,
     width: Dimensions.get('window').width,
-    margin: '1%',
   },
   text: {
     color: '#1b262c',
     fontSize: 25,
     textAlign: 'center',
     marginHorizontal: '1%',
+    marginVertical: '2%',
+  },
+  description: {
+    color: '#1b262c',
+    fontSize: 18,
+    textAlign: 'center',
+    marginHorizontal: '1%',
   },
   link: {
     marginTop: '3%',
     color: '#f0a500',
-    fontSize: 20,
+    fontSize: 23,
     fontWeight: 'bold',
     textAlign: 'center',
     marginHorizontal: '1%',
