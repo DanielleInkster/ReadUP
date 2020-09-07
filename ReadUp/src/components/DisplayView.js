e479ac6fa4aa52d80f6fdf077684fe73b7040fec
DanielleInkster <danielle.inkster@gmail.com>
2020-09-07 17:28:49 -0600

    Added some styling

--- ReadUp/src/components/DisplayView.js
+++ ReadUp/src/components/DisplayView.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import {View, Text, Image, Dimensions} from 'react-native';
+import {View, Text, Image, Dimensions, StyleSheet} from 'react-native';
 import {withDatabase} from '@nozbe/watermelondb/DatabaseProvider';
 import withObservables from '@nozbe/with-observables';
 import Carousel from 'react-native-snap-carousel';
 
 const _renderItem = ({item, index}) => {
   return (
-    <View key={index}>
-      {console.log(item)}
-      <Text>{item.image}</Text>
-      <Text>{item.title}</Text>
+    <View key={index} style={styles.container}>
+      <Image style={styles.image} source={{uri: item.image}} />
+      <Text style={styles.text}>{item.title}</Text>
     </View>
   );
 };
@@ -20,10 +19,30 @@ const DisplayView = ({articles}) => {
       data={articles}
       renderItem={_renderItem}
       sliderWidth={Dimensions.get('window').width}
-      itemWidth={Dimensions.get('window').width * 0.9}
+      itemWidth={Dimensions.get('window').width}
+      containerCustomStyle={styles.container}
     />
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#bbe1fa',
+    marginTop: '3%',
+    marginBottom: '40%',
+  },
+  image: {
+    height: Dimensions.get('window').width,
+    width: Dimensions.get('window').width,
+  },
+  text: {
+    color: '#1b262c',
+    fontSize: 25,
+    textAlign: 'center',
+    marginHorizontal: '1%',
+  },
+});
 
 export default withDatabase(
   withObservables([], ({database}) => ({
