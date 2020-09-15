6971ba80bf2d20aa8ea39b03f61e3b650b3360b2
DanielleInkster <danielle.inkster@gmail.com>
2020-09-15 00:14:01 -0600

    Entry will only be created for valid url

--- ReadUp/src/containers/Edit.js
+++ ReadUp/src/containers/Edit.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import EditView from '../components/EditView';
 import {useDatabase} from '@nozbe/watermelondb/hooks';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Alert} from 'react-native';
 import Cheerio from 'cheerio-without-node-native';
 
 export default function Edit() {
+  const validator = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
   const database = useDatabase();
   const articlesCollection = database.get('articles');
+
+  function checkValidity(text) {
+    return validator.test(text);
+  }
 
   async function scrapeData(text) {
     const searchUrl = text;
@@ -45,11 +50,23 @@ export default function Edit() {
   }
 
   async function createEntry(text) {
-    const data = await scrapeData(text);
-    const title = await getInfo(data, 'title');
-    const description = await getInfo(data, 'description');
-    const image = await getInfo(data, 'image');
-    createDBEntry(title, description, image, text);
+    if (checkValidity(text) === true) {
+      const data = await scrapeData(text);
+      const title = await getInfo(data, 'title');
+      const description = await getInfo(data, 'description');
+      const image = await getInfo(data, 'image');
+      createDBEntry(title, description, image, text);
+    } else {
+      Alert.alert(
+        'Please enter a valid url',
+        [
+          {
+            text: 'OK',
+          },
+        ],
+        {cancelable: false},
+      );
+    }
   }
 
   async function deleteEntry(article) {
