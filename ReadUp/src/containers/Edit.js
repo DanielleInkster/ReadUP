b56616773704bcfb0e30033371bdd74106388e6d
DanielleInkster <danielle.inkster@gmail.com>
2020-09-19 03:19:55 -0600

    Added to alert message

--- ReadUp/src/containers/Edit.js
+++ ReadUp/src/containers/Edit.js
@@ -66,7 +66,7 @@ export default function Edit() {
         });
       });
     } else {
-      createAlert('Database full');
+      createAlert('Database full', 'Maximum of 20 entries');
     }
   }
 
