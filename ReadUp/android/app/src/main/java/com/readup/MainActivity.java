86e422ee784ea51b40f7d811c03f6d2327703629
DanielleInkster <danielle.inkster@gmail.com>
2020-11-01 14:53:48 -0500

    installed react-native-splash-screen

--- ReadUp/android/app/src/main/java/com/readup/MainActivity.java
+++ ReadUp/android/app/src/main/java/com/readup/MainActivity.java
@@ -2,7 +2,17 @@ package com.readup;
 
 import com.facebook.react.ReactActivity;
 
+import org.devio.rn.splashscreen.SplashScreen; 
+
+import com.cboy.rn.splashscreen.SplashScreen;
+
 public class MainActivity extends ReactActivity {
+
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
