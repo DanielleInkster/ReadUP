3033d749a20bee73611895e172cfb654a77fc786
DanielleInkster <danielle.inkster@gmail.com>
2020-11-02 22:17:10 -0500

    basic test for trim function

--- ReadUp/__tests__/CarouselItem.spec.js
+++ ReadUp/__tests__/CarouselItem.spec.js
@@ -11,6 +11,18 @@ const article = {
   url: 'www.fakesite.com',
   image: '../src/images/none.jpg',
 };
+
+const article2 = {
+  id: '2',
+  title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  description:
+    'sed do eiusmod tempor incididunt ut labore et dolore magna aliquaUt enim ad minim veniam,' +
+    'quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' +
+    'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  url: 'www.fakesite.com',
+  image: '../src/images/none.jpg',
+};
+
 let mockOpenURL = jest.fn();
 
 jest.mock('react-native/Libraries/Linking/Linking', () => ({
@@ -29,6 +41,20 @@ describe('CarouselItem', () => {
     const {getByText} = render(<CarouselItem item={article} index={1} />);
     expect(getByText('hello')).not.toBeNull();
     expect(getByText('world')).not.toBeNull();
+  });
+});
+
+describe('trim', () => {
+  it('trims title and description', () => {
+    const {getByText} = render(<CarouselItem item={article2} index={2} />);
+    expect(
+      getByText('Lorem ipsum dolor sit amet, consectetur adipiscing...'),
+    ).not.toBeNull();
+    expect(
+      getByText(
+        'sed do eiusmod tempor incididunt ut labore et dolore magna aliquaUt enim ad minim ven...',
+      ),
+    ).not.toBeNull();
   });
 });
 
