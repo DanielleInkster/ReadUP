7a2dd11171e6758a476a7cbf4e027a49d88cc0fa
DanielleInkster <danielle.inkster@gmail.com>
2020-11-07 16:45:26 -0500

    Added memo to fix list re-rendering

--- ReadUp/src/components/ArticleView.js
+++ ReadUp/src/components/ArticleView.js
@@ -48,4 +48,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default ArticleView;
+export default React.memo(ArticleView);
--- ReadUp/src/components/CarouselItem.js
+++ ReadUp/src/components/CarouselItem.js
@@ -111,4 +111,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default CarouselItem;
+export default React.memo(CarouselItem);
