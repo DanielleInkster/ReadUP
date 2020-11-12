a5231e21366a06817fe346dd25dbe76b0d21f699
DanielleInkster <danielle.inkster@gmail.com>
2020-11-12 17:48:56 -0500

    Moved license info to license.js

---
+++ ReadUp/src/licenses.js
@@ -0,0 +1,56 @@
+export const licenses = [
+  {
+    key: '1',
+    title: 'WatermelonDB',
+    license: 'MIT',
+    url: 'https://github.com/Nozbe/WatermelonDB',
+  },
+  {
+    key: '2',
+    title: 'withObservables',
+    license: 'MIT',
+    url: 'https://github.com/Nozbe/withObservables',
+  },
+  {
+    key: '3',
+    title: 'Cheerio Without Node Native',
+    license: 'MIT',
+    url: 'https://github.com/oyyd/cheerio-without-node-native',
+  },
+  {
+    key: '4',
+    title: 'React Native Gesture Handler',
+    license: 'MIT',
+    url: 'https://github.com/software-mansion/react-native-gesture-handler',
+  },
+  {
+    key: '5',
+    title: 'React Native Snap Carousel',
+    license: 'BSD-3-Clause',
+    url: 'https://github.com/archriss/react-native-snap-carousel',
+  },
+  {
+    key: '6',
+    title: 'React Native Splash Screen',
+    license: 'MIT',
+    url: 'https://github.com/crazycodeboy/react-native-splash-screen',
+  },
+  {
+    key: '7',
+    title: 'React Native Vector Icons',
+    license: 'MIT',
+    url: 'https://github.com/oblador/react-native-vector-icons',
+  },
+  {
+    key: '8',
+    title: 'React Native',
+    license: 'MIT',
+    url: 'https://github.com/facebook/react-native',
+  },
+  {
+    key: '9',
+    title: 'React',
+    license: 'MIT',
+    url: 'https://github.com/facebook/react/raw/master/LICENSE',
+  },
+];
