be05f4b8819a875ec2f71cfc8afc8881ce50a809
DanielleInkster <danielle.inkster@gmail.com>
2020-11-12 17:33:09 -0500

    Finished basic attributions page

--- ReadUp/src/components/AttributionsItem.js
+++ ReadUp/src/components/AttributionsItem.js
@@ -1,33 +1,41 @@
 import React from 'react';
-import {View, Text, Linking, StyleSheet, TouchableOpacity} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Linking,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 Icon.loadFont();
 
-const AttributionsItem = ({title, creator, url, icon}) => {
+const AttributionsItem = ({title, license, url}) => {
   return (
     <TouchableOpacity style={styles.articleItem}>
-      <View style={styles.articleItemView}>
+      <SafeAreaView style={styles.articleItemView}>
+        <View style={styles.articleTextView}>
+          <Text style={styles.heading}>{title}</Text>
+          <Text style={styles.text}>Provided under the {license} license.</Text>
+        </View>
         <Icon
-          name={icon}
+          name="chevron-right"
           style={styles.icon}
           onPress={() => Linking.openURL(url)}
         />
-        <Text style={styles.text}>{title}</Text>
-        <Text style={styles.text}>{creator}</Text>
-      </View>
+      </SafeAreaView>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   articleItem: {
-    padding: 20,
+    padding: 10,
     borderRadius: 10,
     backgroundColor: '#bbe1fa',
     borderBottomWidth: 1,
-    borderColor: '#eee',
-    marginVertical: '3%',
+    borderColor: 'slategray',
     marginHorizontal: '2%',
   },
   articleItemView: {
@@ -35,14 +43,25 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
   },
+  articleTextView: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+  },
   text: {
-    color: '#1b262c',
-    fontSize: 18,
+    color: '#0f4c75',
+    fontSize: 12,
+    fontStyle: 'italic',
+    fontFamily: 'sans-serif-medium',
+  },
+  heading: {
+    color: '#0f4c75',
+    fontSize: 14,
     fontFamily: 'sans-serif-medium',
   },
   icon: {
-    fontSize: 25,
-    color: 'grey',
+    fontSize: 20,
+    color: '#0f4c75',
   },
 });
 
--- ReadUp/src/pages/About.js
+++ ReadUp/src/pages/About.js
@@ -1,28 +1,75 @@
 import React from 'react';
-import {View, FlatList, StyleSheet} from 'react-native';
+import {View, Text, FlatList, StyleSheet} from 'react-native';
 import AttributionsItem from '../components/AttributionsItem';
 
 const data = [
   {
-    key: 1,
-    title: 'My project',
-    creator: 'Dani Inkster',
-    url: 'http://www.google.com',
-    icon: 'link',
+    key: '1',
+    title: 'WatermelonDB',
+    license: 'MIT',
+    url: 'https://github.com/Nozbe/WatermelonDB',
+  },
+  {
+    key: '2',
+    title: 'withObservables',
+    license: 'MIT',
+    url: 'https://github.com/Nozbe/withObservables',
+  },
+  {
+    key: '3',
+    title: 'Cheerio Without Node Native',
+    license: 'MIT',
+    url: 'https://github.com/oyyd/cheerio-without-node-native',
+  },
+  {
+    key: '4',
+    title: 'React Native Gesture Handler',
+    license: 'MIT',
+    url: 'https://github.com/software-mansion/react-native-gesture-handler',
+  },
+  {
+    key: '5',
+    title: 'React Native Snap Carousel',
+    license: 'BSD-3-Clause',
+    url: 'https://github.com/archriss/react-native-snap-carousel',
+  },
+  {
+    key: '6',
+    title: 'React Native Splash Screen',
+    license: 'MIT',
+    url: 'https://github.com/crazycodeboy/react-native-splash-screen',
+  },
+  {
+    key: '7',
+    title: 'React Native Vector Icons',
+    license: 'MIT',
+    url: 'https://github.com/oblador/react-native-vector-icons',
+  },
+  {
+    key: '8',
+    title: 'React Native',
+    license: 'MIT',
+    url: 'https://github.com/facebook/react-native',
+  },
+  {
+    key: '9',
+    title: 'React',
+    license: 'MIT',
+    url: 'https://github.com/facebook/react/raw/master/LICENSE',
   },
 ];
 
-export default function About({articles}) {
+export default function About() {
   return (
     <View style={styles.container}>
+      <Text style={styles.heading}>Attributions</Text>
       <FlatList
         data={data}
         renderItem={({item}) => (
           <AttributionsItem
             title={item.title}
-            creator={item.creator}
+            license={item.license}
             url={item.url}
-            icon={item.icon}
           />
         )}
       />
@@ -33,5 +80,14 @@ export default function About({articles}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: '5%',
+  },
+  heading: {
+    color: '#bbe1fa',
+    fontSize: 20,
+    fontWeight: 'bold',
+    fontFamily: 'sans-serif-medium',
+    alignSelf: 'center',
+    paddingBottom: '2%',
   },
 });
